0dfac385ef4483a385124ef50713a90731a8985e
Nishigandha20 <shindenisha2696@gamil.com>
2023-01-04 19:19:22 +0530

    fisrt comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { MenupageComponent } from './pages/menupage/menupage.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'menu/:id',component:MenupageComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
